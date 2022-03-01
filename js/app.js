957971c60fb27a2552c4104bf369a8229ae5bd20
Luis Blanco <luiswblancod@gmail.com>
2022-02-28 19:46:39 -0500

    app terminada y comentarios listos

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// function coupon discount (esta funcion maneja los cupones de descuento)
 function couponDiscount() {
   const coupons = ["user_coupon_discount", "LuchoDev", "app_discount_coupon"];
 
@@ -13,13 +14,14 @@ function couponDiscount() {
     case coupons[1]:
       descuentoCupon = 30;
       break;
-    case coupons[2]: // "es_un_secreto"
-      descuentoCupon = 25; // 25%
+    case coupons[2]:
+      descuentoCupon = 25;
       break;
   }
   return descuentoCupon;
 }
 
+// function suma los porcentajes (esta funcion es la sumatoria del porcentaje colocado y el cupon de descuento)
 function sumPorcentage(discount = 0, coupon = 0) {
   const sumaDiscountCoupon = discount + coupon;
   const porcentageDiscountCoupon = (100 - sumaDiscountCoupon) / 100;
@@ -27,6 +29,7 @@ function sumPorcentage(discount = 0, coupon = 0) {
   return porcentageDiscountCoupon;
 }
 
+// function suma los porcentajes pero los arroja simples "esto es para cuanto dinero sobra"
 function priceSaving(discount = 0, coupon = 0) {
   const sumaSaving = discount + coupon;
   const porcentageSaving = sumaSaving / 100;
@@ -34,27 +37,7 @@ function priceSaving(discount = 0, coupon = 0) {
   return porcentageSaving;
 }
 
-// function resultSavingTotal() {
-//   if (inputDiscount.value.length === 0 && inputCoupon.value.length === 0) {
-//     const resultSavingTotal = "No hay Descuento.";
-//     const resultViewTotal = (resultSaving.textContent =
-//       "Esta ahorrando $" + resultSavingTotal);
-//     return resultViewTotal;
-//   } else if (inputDiscount.value.length === 0) {
-//     const resultSavingCouponValue = priceValue * priceSaving(couponDiscount());
-//     return (resultSaving.textContent =
-//       "Con su CUPON de Descuento, Esta ahorrando $" + resultSavingCouponValue);
-//   } else if (inputCoupon.value.length === 0) {
-//     const resultSavingDiscountValue = priceValue * priceSaving(discountValue);
-//     return (resultSaving.textContent =
-//       "Con el DESCUENTO del  Esta ahorrando $" + resultSavingDiscountValue);
-//   } else {
-//     const priceSavingTotal =
-//       priceValue * priceSaving(discountValue, couponDiscount());
-//     return (resultSaving.textContent = "Esta ahorrando $" + priceSavingTotal);
-//   }
-// }
-
+// function activador del click (Aca esta todo el manejo y funcionalidad, al darle click al boton "onClickButtonPriceDiscountCoupon")
 function onClickButtonPriceDiscountCoupon() {
   const resultPrice = document.getElementById("resultPrice");
   const resultSaving = document.getElementById("resultSaving");
@@ -89,13 +72,9 @@ function onClickButtonPriceDiscountCoupon() {
     return (resultPrice.textContent =
       "Total + Descuento + Cupon: " + "$" + priceDiscount);
   }
-
-  // const priceSavingTotal =
-  //   priceValue * priceSaving(discountValue, couponDiscount());
-
-  // resultSaving.textContent = "Esta ahorrando $" + priceSavingTotal;
 }
 
+// function activador del click en el boton (Aca esta todo el manejo y funcionalidad de cuanto dinero queda, al darle click al boton "onClickButtonResultSaving")
 function onClickButtonResultSaving() {
   const resultPrice = document.getElementById("resultPrice");
   const resultSaving = document.getElementById("resultSaving");
